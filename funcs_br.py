e2383fd5a4f35f79ebe36fabbc1d76920e520761
ashkanpakzad <ashpakzad@gmail.com>
2019-12-06 17:30:44 +0000

    made plot_3d function more versatile to accept just a binary image as well as verticies

--- funcs_br.py
+++ funcs_br.py
@@ -21,12 +21,15 @@ def save_image(array, affine, header, namepath):
     nib.save(nib.Nifti1Image(array, affine, header), namepath + '.nii.gz')
     return
 
-
-def plot_3d(image, graph_vertices, threshold):
+def plot_3d(*args, threshold=0):
+    ### POSITIONAL ARGS respectively:
+    # required: image.
+    # optional: graph verticies
+    
     # TODO - figure the proper orientation out
     # Position the scan upright,
     # so the head of the patient would be at the top facing the camera
-    p = image #.transpose(2, 1, 0)
+    p = args[0].T #.transpose(2, 1, 0)
     # p = p[:, :, ::-1]
 
     verts, faces = measure.marching_cubes_classic(p, threshold)
@@ -39,10 +42,24 @@ def plot_3d(image, graph_vertices, threshold):
     face_color = [0.5, 0.5, 1]
     mesh.set_facecolor(face_color)
     ax.add_collection3d(mesh)
-    ax.scatter(graph_vertices[..., 0], graph_vertices[..., 1], graph_vertices[..., 2], s=30, c='red', marker='o')
+    if len(args) == 2:
+        graph_vertices = args[1]
+        ax.scatter(graph_vertices[..., 2], graph_vertices[..., 1], graph_vertices[..., 0], s=30, c='red', marker='o')
 
     ax.set_xlim(0, p.shape[0])
     ax.set_ylim(0, p.shape[1])
     ax.set_zlim(0, p.shape[2])
 
+    plt.show()
+    
+    
+def isosurface(binary_array):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    pos = np.where(binary_array==1)
+    
+    ax.scatter(pos[0], pos[1], pos[2], c='black')
+    ax.set_xlim(0, binary_array.shape[0])
+    ax.set_ylim(0, binary_array.shape[1])
+    ax.set_zlim(0, binary_array.shape[2])
     plt.show()
