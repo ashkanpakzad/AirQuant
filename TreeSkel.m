98b0ab06ef0fe8124cb80bed79ce28df9f1be7a6
ashkanpakzad <ashpakzad@gmail.com>
2020-03-20 06:49:55 +0000

    initialised skeletonisation algorithm with algorithm components

---
+++ TreeSkel.m
@@ -0,0 +1,168 @@
+function TreeSkel(object)
+
+
+
+
+function h = circle(x,y,r)
+hold on
+th = 0:pi/50:2*pi;
+xunit = r * cos(th) + x;
+yunit = r * sin(th) + y;
+h = plot(xunit, yunit, 'r');
+hold off
+end
+
+function LSF = sigfactor(px, py, object, DTmap)
+nb = neighbors(px, py);
+
+inequalityvals = zeros(length(nb),1);
+for i = 1:length(nb)
+    dist = abs(px-nb(i,1))+abs(py-nb(i,2));
+    DTval = DTmap(px, py);
+    upper = DTmap(nb(i,1), nb(i,2)) - DTval;
+    lower = 0.5*(object(px, py)+object(nb(i,1), nb(i,2)))*dist;
+    inequalityvals(i) = upper/lower;
+end
+output = abs(max(inequalityvals(:)));
+if output > 0
+    LSF = 1- output;
+else
+    LSF = 1;
+end
+end
+
+function SC = stepcost(px,py,qx,qy,object,DTmap)
+
+epsilon = 0.01;
+LSF1 = sigfactor(px, py, object, DTmap);
+LSF2 = sigfactor(qx, qy, object, DTmap);
+dist = abs(px-qx)+abs(py-qy);
+
+SC = dist/(epsilon+(LSF1+LSF2)^2);
+end
+
+function nb = neighbors(px, py)
+[Qx, Qy] = ndgrid(px-1:px+1, py-1:py+1);
+nb = [Qx(:),Qy(:)];
+nb(5,:) = [];
+end
+
+function [pathX,pathY,cost] = mincostpath(sx, sy, tx, ty, object, DTmap)
+
+startind = sub2ind(size(object), sx, sy);
+stopind = sub2ind(size(object), tx, ty);
+
+% convert image grid to graph
+g = binaryImageGraph(object);
+%plotImageGraph(g)
+
+% compute step cost across grid
+for i = 1:height(g.Edges)
+    pnode = g.Edges.EndNodes(i,1);
+    qnode = g.Edges.EndNodes(i,2);
+    py = g.Nodes.x(pnode);
+    px = g.Nodes.y(pnode);
+    qy = g.Nodes.x(qnode);
+    qx = g.Nodes.y(qnode);
+    g.Edges.Weight(i) = stepcost(px,py,qx,qy,object,DTmap);
+end
+
+% compute lowest cost path
+startnode = find(g.Nodes.PixelIndex == startind);
+stopnode = find(g.Nodes.PixelIndex == stopind);
+
+[P, cost] = shortestpath(g, startnode, stopnode);
+
+% get coords
+[pathX,pathY] = ind2sub(size(object), g.Nodes.PixelIndex(P));
+end
+
+
+function Bmarked = adaptivefill(Bskel, object, DTmap)
+% Bskel = skeleton of branch only
+
+skel = zeros(size(object));
+skel(Bskel) = 1;
+% identify object but not skeleton voxels.
+nonskel = zeros(size(object));
+nonskel(object == 1 & skel == 0) = 1;
+
+% initialise with Dilation Scale map
+DSmap = zeros(size(object));
+DSmap(Bskel) = DTmap(Bskel);
+DSmap(nonskel == 1) = -1*max(DSmap(:));
+
+% identify all p part of object but not the skeleton
+allp = find(nonskel == 1);
+[allpx, allpy] = ind2sub(size(nonskel), allp);
+
+%copy DSmap
+DSmapprev = DSmap;
+DSmapnew = zeros(size(DSmap)); % holder!
+k = 0;
+while ~isequal(DSmapnew, DSmapprev) % while there is no change
+    % dummy on very first iteration
+    if k == 0
+        DSmapnew = DSmap;
+    end
+    %update iteration
+    k = k+1;
+    % shift current to previous
+    DSmapprev = DSmapnew;
+    % loop through all voxels in object but not in skel
+    for j = 1:length(allp)
+        
+        px = allpx(j);
+        py = allpy(j);
+        
+        % identify neighbor
+        nb = neighbors(px, py);
+        
+        dsvals = zeros(length(nb),1);
+        for i = 1:length(nb)
+            %dist = abs(px-nb(i,1))+abs(py-nb(i,2));
+            dist = sqrt((px-nb(i,1))^2+(py-nb(i,2))^2);
+            dsvals(i) = DSmapprev(nb(i,1), nb(i,2)) - dist;
+        end
+        % update DSmap
+        DSmapnew(px,py) = max(dsvals(:));
+    end
+end
+
+Bmarked = (DSmapnew >= 0 & object == 1);
+end
+
+function [furthestCMBX, furthestCMBY] = furthestCMB(Omarked,CMB_list, subtree_ind)
+% subtree_ind = cell2mat(CC_unmarked.PixelIdxList(B_potential(1)));
+% Omarked = Omarked;
+% CMB_list= T.linear;
+
+% subtree_ind = list of linear indicies in the subtree
+% Omarked = marked volume of skel algorithm
+% CMB_list = list linear indicies of Central Maximal Balls
+
+% get boundary voxels of Omarked
+Omarked_boundcell = bwboundaries(Omarked);
+Omarked_bound = Omarked_boundcell{1,1};
+
+% get subset of CMB in subtree
+commonCMBind = intersect(CMB_list, subtree_ind);
+
+% compute distances of all Omarked boundary to all CMB in T_i
+[commonCMBlinY, commonCMBlinX]= ind2sub(size(Omarked), commonCMBind);
+
+disti = zeros(length(commonCMBind),1);
+for i = 1:length(commonCMBind)
+    distj = zeros(length(Omarked_bound),1);
+    for j = 1:length(Omarked_bound)
+        distj(j,1) = sqrt((commonCMBlinX(i,1) - Omarked_bound(j,1))^2 + (commonCMBlinY(i,1) - Omarked_bound(j,2))^2);
+    end
+    [disti(i,1)] = max(distj(:));
+end
+        
+% extract max distance and return furthest CMB
+[~,furthestCMBcommon] = max(disti);
+furthestCMBX = commonCMBlinX(furthestCMBcommon);
+furthestCMBY = commonCMBlinY(furthestCMBcommon);
+end
+end
