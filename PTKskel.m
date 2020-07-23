d35ff1479d3e43bdb08d05813680ca5b50c1224f
ashkanpakzad <ashpakzad@gmail.com>
2020-07-23 09:18:53 +0100

    output only saves image as nii.gz using PTK interface

--- PTKskel.m
+++ PTKskel.m
@@ -4,14 +4,14 @@
 % SegImagepath can be any valid image type accepted by PTK e.g. nifti,
 % nrrd etc. see github.com/tomdoel/pulmonarytoolkit for details
 
-function result = PTKskel(SegImagePath, PTK_source)
+function PTKskel(SegImagePath, PTK_source)
 % SegImagePath is the path to the segmentation image
 % PTK_source is the source directory of PTK which need not be given if PTK
 % is already in the MATLAB search path.
 
 % add PTK main directory to path if necessary
-if nargin > 1
-    addpath(PTK_source)
+if nargin > 2
+    addpath(PTK_source);
 end
 PTKAddPaths;
 % prevent PTK from creating dialog boxes
@@ -19,13 +19,28 @@ reporting = CoreReporting([], false);
 ptk_main = PTKMain(reporting);
 
 % unzip and load images
-if strcmp(SegImagePath(end-3:end), '.gz') == 1
-    gunzip(SegImagePath)
+if strcmp(SegImagePath(end-2:end), '.gz') == 1
+    compressed = 1;
+    gunzip(SegImagePath);
     SegImagePath = SegImagePath(1:end-3);
+else
+    compressed = 0;
 end
 
 seg_data = ptk_main.Load(SegImagePath);
 
-% get image
 skel = seg_data.GetResult('PTKCustomAirwayCentreline');
-result = skel.RawImage;
+
+% save result
+[filepath,name,ext] = fileparts(SegImagePath);
+savename = [name, '_PTKskel.nii'];
+
+MimSaveAsNifti(skel, filepath, savename, reporting);
+% compress to .nii.gz
+gzip(fullfile(filepath,savename))
+delete(fullfile(filepath,savename))
+
+% delete decompression of seg image
+if compressed == 1
+    delete(fullfile(filepath,[name,ext]))
+end
