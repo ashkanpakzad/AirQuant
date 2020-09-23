9727d2e2a51e2c7fe1d856c4ad8c93afef8a517e
ashkanpakzad <ashpakzad@gmail.com>
2020-09-23 16:23:46 +0100

    create results dir

--- AirQuantAddPath.m
+++ AirQuantAddPath.m
@@ -1,3 +1,10 @@
 function AirQuantDir = AirQuantAddPath()
 [AirQuantDir,~,~] = fileparts(which('AirQuantAddPath'));
 addpath(genpath(AirQuantDir))
+
+results_dir = fullfile(AirQuantDir, 'results');
+
+if ~isfolder(results_dir)
+    mkdir(results_dir)
+end
+end
