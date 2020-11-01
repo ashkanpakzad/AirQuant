62890c66ee7ac44d96bcafbe6aedb5f95108b347
ashkanpakzad <ashpakzad@gmail.com>
2020-11-01 14:17:27 +0000

    added for loop for capturing multiple expmeriments with parameter change

--- scripts/phantomanalysis.m
+++ scripts/phantomanalysis.m
@@ -9,26 +9,22 @@ AirQuantDir = AirQuantAddPath();
 results_dir = fullfile(AirQuantDir,'results');
 recontype = 'Lung'; % phantom reconstruction kernel. Lung or Body.
 phantomtype = 'constant'; % taper or constant
-extra = 'planesample0p6';
 
-casename = ['Phantom_',recontype,'_',phantomtype,'_',extra];
-
-plane_sample_sz = 0.6; % Set parameters for AQ
-
+samplesizes = 0.1:0.1:1;
 
 % fixed points for taper tubes
-% I = [   335, 214, 179; 335, 215, 124; 
-%         294, 201, 179; 295, 201, 124; 
-%         350, 174, 179; 349, 176, 124; 
+% I = [   335, 214, 179; 335, 215, 124;
+%         294, 201, 179; 295, 201, 124;
+%         350, 174, 179; 349, 176, 124;
 %         375, 227, 179; 372, 226, 124;
 %         323, 251, 179; 323, 249, 124;   ];
 
 % fixed points for constant tubes
 I = [   243, 362, 179; 246, 362, 124;
-        213, 360, 179; 215, 360, 124;
-        184, 359, 179; 187, 358, 124;
-        215, 333, 179; 217, 333, 124;
-        211, 387, 179; 213, 387, 124;   ];
+    213, 360, 179; 215, 360, 124;
+    184, 359, 179; 187, 358, 124;
+    215, 333, 179; 217, 333, 124;
+    211, 387, 179; 213, 387, 124;   ];
 
 %% Get filenames
 
@@ -50,43 +46,51 @@ skel = logical(SkelPTK_windex(S, fixedpoints));
 %% Initialise AirQuant
 % Parses CT, segmentation and skeleton to compute airway tree graph
 % savename is given to automatically save/load results.
-savename = fullfile(results_dir, [casename, '_AQ.mat']);
-AQ = AirQuantPhantom(CT, meta, S, skel, savename);
-AQ.plane_sample_sz = plane_sample_sz;
-
-%% Display Airway Graph in 3D and 2D
-% figure
-% PlotTree(AQ)
-% 
-% figure
-% plot(AQ,'gen')
-% 
-% Plot the skeleton inside the segmentation
-% skel = logical(Skeletonise_via_PTK_with_index(S, fixedpoints));
-% figure
-% patch(isosurface(S),'EdgeColor', 'none','FaceAlpha',0.3);
-% hold on
-% isosurface(skel)
-% axis vis3d
-% ax = gca;
-% ax.XDir = 'reverse';
-
-%% traverse all airways
-% this could take a number of hours, results will be saved along the way so
-% can be interrupted and rerun at a later time.
-tic;
-AQ = AirwayImageAll(AQ);
-% show how long it took
-time = toc;
-disp(toc/60)
-
-%% compute area of all airways
-% This should only take a few minutes and automatically saves once all
-% measurements are complete.
-AQ = FindFWHMall(AQ);
-save(AQ)
-
-%% This will tell you which branches have been processed.
-% if both the AirwayImageAll and FindFWHMall functions have been run to
-% completion successfully then the results of this should show all 1s.
-report = debuggingreport(AQ);
+for ii = 1:length(samplesizes)
+    plane_sample_sz = samplesizes(ii);
+    text = sprintf('%1.1f',plane_sample_sz);
+    extra = ['planesample_',text(1),'_',text(3)];
+    
+    casename = ['Phantom_',recontype,'_',phantomtype,'_',extra];
+    
+    savename = fullfile(results_dir, [casename, '_AQ.mat']);
+    AQ = AirQuantPhantom(CT, meta, S, skel, savename);
+    AQ.plane_sample_sz = plane_sample_sz;
+    
+    %% Display Airway Graph in 3D and 2D
+    % figure
+    % PlotTree(AQ)
+    %
+    % figure
+    % plot(AQ,'gen')
+    %
+    % Plot the skeleton inside the segmentation
+    % skel = logical(Skeletonise_via_PTK_with_index(S, fixedpoints));
+    % figure
+    % patch(isosurface(S),'EdgeColor', 'none','FaceAlpha',0.3);
+    % hold on
+    % isosurface(skel)
+    % axis vis3d
+    % ax = gca;
+    % ax.XDir = 'reverse';
+    
+    %% traverse all airways
+    % this could take a number of hours, results will be saved along the way so
+    % can be interrupted and rerun at a later time.
+    tic;
+    AQ = AirwayImageAll(AQ);
+    % show how long it took
+    time = toc;
+    disp(toc/60)
+    
+    %% compute area of all airways
+    % This should only take a few minutes and automatically saves once all
+    % measurements are complete.
+    AQ = FindFWHMall(AQ);
+    save(AQ)
+    
+    %% This will tell you which branches have been processed.
+    % if both the AirwayImageAll and FindFWHMall functions have been run to
+    % completion successfully then the results of this should show all 1s.
+    report = debuggingreport(AQ);
+end
