dda400569b8dfa22e5e7f1f8a8b9d17ae9d95673
Ashkan Pakzad <ashpakzad@gmail.com>
2021-02-09 18:24:09 +0000

    Removed redundant long airway tapering analysis. Can be accessed in seperate script to be added later. Added in new generation count funcitons.

--- scripts/example.m
+++ scripts/example.m
@@ -4,8 +4,8 @@
 % names input files, give fullpaths if not in matlab path
 % add AirQuant library to path
 AirQuantDir = AirQuantAddPath();
-results_dir = fullfile(AirQuantDir,'results');
 casename = 'github_demo';
+results_dir = fullfile(AirQuantDir,'results', casename);
 
 %% Get filenames
 CT_name = [casename, '_raw.nii.gz'];
@@ -23,7 +23,7 @@ skel = logical(niftiread(skel_name));
 %% Initialise AirQuant
 % Parses CT, segmentation and skeleton to compute airway tree graph
 % savename is given to automatically save/load results.
-savename = fullfile(results_dir, [casename, '_AQ.mat']);
+savename = fullfile(results_dir, [casename, '_AQ15.mat']);
 AQ = AirQuant(CT, meta, S, skel, savename);
 
 %% Display Airway Graph in 3D and 2D
@@ -34,21 +34,14 @@ figure
 plot(AQ)
 
 %% compute lobes
-AQ = ComputeAirwayLobes(AQ);
-figure; PlotMap3D(AQ, 'Lobe');
+figure; PlotMap3D(AQ, 'lobe');
 % Generatlly use the save function when AQ given and returned by function.
-save(AQ) 
-% MATLAB's colourcoding can be a little buggy when viewing results.
 % Note that the upper left lobe's lingular is treated seperately. An
 % emulation of the middle lobe in the right lung. The Airway segmentation
 % must reach all anatomical lobes for this function to be successful.
 
 %% Plot the skeleton inside the segmentation
-figure
-patch(isosurface(S),'EdgeColor', 'none','FaceAlpha',0.3);
-hold on
-isosurface(skel)
-axis vis3d
+figure; PlotSegSkel(AQ);
 
 %% traverse the first indexed airway, measure and display results
 % this may take >20 minutes
@@ -56,7 +49,7 @@ axis vis3d
 % identify branch indices) this function interpolates slices along that
 % branch and then attempts to fit an ellipse to the inner lumen, wall peak
 % attenuation and outer wall boundaries.
-idx = 26;
+idx = 24;
 tic;
 AQ = CreateAirwayImage(AQ, idx);
 AQ = FindAirwayBoundariesFWHM(AQ, idx);
@@ -67,6 +60,7 @@ disp(toc/60)
 % the original image is greater than this then the interpolated slices will
 % be noticibly blurry and results poor.
 PlotAirway3(AQ, idx)
+OrthoViewAirway(AQ, idx)
 
 % AirQuant doesn't save tapering measurements automatically 
 % (only interpolated slices), so we can manually invoke the save method after
@@ -91,30 +85,9 @@ save(AQ)
 %% This will tell you which branches have been processed.
 % if both the AirwayImageAll and FindFWHMall functions have been run to
 % completion successfully then the results of this should show all 1s.
-report = debuggingreport(AQ);
+SuccessReport(AQ);
 
-%% Compute taper gradient path of all terminal branches
-% Table with all carina-terminal branch data.
-% running ComputeAirwayLobes() before this function will group output by lobe.
-AllTaperResults = ComputeTaperAll(AQ);
+%% See number of airways per generation
+figure; AirwayCounts(AQ, 'generation')
+figure; AirwayCounts(AQ, 'lobe')
 
-%% Display taper results of a single gradient path
-% plot taper grad results from carina to a given end-node.
-figure
-PlotTaperResults(AQ, AllTaperResults.terminalnode(1), 'inner')
-figure
-PlotTaperResults(AQ, AllTaperResults.terminalnode(1))
-
-%% Construct single taper gradient path
-% this demonstrates more hands on functions to process data as desired.
-% list of terminal node, refer to airway graph.
-terminalnodelist = ListTerminalNodes(AQ);
-% get branch data for carina to terminal node for given node.
-% note a positive taper gradient shows that the branch is tapering.
-[logtaperrate, cum_arclength, cum_area, path] = ConstructTaperPath(AQ, terminalnodelist(1)); 
-
-%% Display boxplot of tapervalues by lobe
-figure
-TaperBoxPlot(AQ, 'inner')
-figure
-TaperBoxPlot(AQ)
