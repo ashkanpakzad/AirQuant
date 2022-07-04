2d7566af9ea10a4fff410b70a16dd5398602e4e9
ashkanpakzad <ashpakzad@gmail.com>
2022-07-04 21:43:10 +0100

    refactored ptkskel to use seeding point if required

--- library/PTKskel/PTKskel.m
+++ library/PTKskel/PTKskel.m
@@ -3,28 +3,47 @@
 % Get skeleton given airway segmentation using PTK removing internal loops
 % SegImagepath can be any valid image type accepted by PTK e.g. nifti,
 % nrrd etc. see github.com/tomdoel/pulmonarytoolkit for details
+%
+% Seed is 3x1, [x,y,z] vector of coordinates in LPS coordinates. It is
+% converted into LPI for compatibility with PTK standard image format. and
+% first two axes swapped to image space [row, col].
+%
 
-function skelvol = PTKskel(SegImage, saveflag)
+function skelvol = PTKskel(segpath, seed, saveflag)
 % SegImagePath is the path to the segmentation image
 % PTK_source is the source directory of PTK which need not be given if PTK
 % is already in the MATLAB search path.
 % add PTK main directory to path if necessary
+
 if nargin < 2
+    seed = [];
+end
+
+if nargin < 3
     saveflag = 1;
 end
-% prevent PTK from creating dialog boxes
+
+% init PTK
 ptk_main = PTKMain();
 
-% load in segmentation and execute
+% get full segpath and load
+segpath_full = which(segpath);
+seg_data = ptk_main.Load(segpath_full);
+seg_ptk = seg_data.GetResult('PTKOriginalImage');
 
-seg_data = ptk_main.Load(SegImage);
+if isempty(seed) 
+% get seed by trachea identification if not specified
+    [seed, ~] = PTKFindTopOfTrachea(seg_ptk, ptk_main.Reporting, false);
+else
+    % swap axis 1 and 2 and convert to LPI
+    imsz = seg_ptk.OriginalImageSize;
+    seed = [seed(2), seed(1), imsz(3) - seed(3) + 1];
+end
 
-skel = seg_data.GetResult('PTKCustomAirwayCentreline');
+skel_ptk = SeededCentreline(ptk_main, seg_ptk, seed);
 
 % save result if requested and segmentation loaded originally.
-
-
-[filepath,name,ext] = fileparts(SegImage);
+[filepath,name,~] = fileparts(segpath_full);
 
 if endsWith(name, '.nii')
     name = name(1:end-4);
@@ -32,18 +51,14 @@ end
 
 if saveflag == 1
     savename = [name, '_PTKskel.nii'];
-    
-    MimSaveNiftiAsNifti(skel, filepath, savename, ptk_main.Reporting);
+    MimSaveNiftiAsNifti(skel_ptk, filepath, savename, ptk_main.Reporting);
     % compress to .nii.gz
     gzip(fullfile(filepath,savename))
-    delete(fullfile(filepath,savename))
-    
-    % delete decompression of seg image
-    
+    delete(fullfile(filepath,savename))    
 end
 
 if nargout > 0
-    skelvol = skel.RawImage;
-    % Test output form.
+    skelvol = skel_ptk.RawImage;
+    % from PTK orientation to original
     skelvol = permute(flip(skelvol,3),[2,1,3]);
 end
