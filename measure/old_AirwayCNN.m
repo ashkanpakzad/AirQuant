ad968d7b8877d0bc6d9852124392d4d2d537829b
ashkanpakzad <ashpakzad@gmail.com>
2023-02-24 12:15:41 +1100

    updated airwayCNN to version with seg

--- measure/old_AirwayCNN.m
+++ measure/old_AirwayCNN.m
@@ -3,50 +3,76 @@ classdef AirwayCNN < SuperMeasure
         function obj = AirwayCNN(varargin)
             % See superclass constructor method SuperMeasure.
             %
+            % long desc
+            %
+            % .. todo: add documentation to this function
+            %
+            % Args:
+            %   x(type):
+            %
+            % Return:
+            %   y(type):
             %
             obj@SuperMeasure(varargin{:});
         end
 
-        function Measure(obj, model_path)
-            % Measure airways using a deep learning regressor.
+        function Measure(obj, modulepath, model_path, subtype)
+            % Measure airways using a deep learning method
+            %
+            % long desc
+            %
+            % .. todo: add documentation to this function
+            %
+            % Args:
+            %   modulepath(char): absolute path to awyGAN parent directory.
+            %   model_path(char): absolute path to model load checkpoint.
             %
             %
             arguments
                 obj
+                modulepath char
                 model_path char
+                subtype char {mustBeMember(subtype,{'awyGAN','CNR'})} = 'CNR'
             end
-
+            
+            % subtype
+            switch subtype
+                case 'awyGAN'
+                    subtype = 'runawyGANmodel';
+                case 'CNR'
+                    subtype = 'runCNNmodel';
+            end
+               
 
             % save patches to temp dir
             obj.pixsize = 0.5;
             temp_dir = tempname;
-            temp_dir = [temp_dir, '.tar'];
             obj.tube.ExportOrthoPatches(temp_dir, 'temp')
 
             % compute image stats
             M = cell2mat(obj.tube.source);
             meanval = mean(M(:));
             varval = std(M(:));
-
+            
             % add module path to python paths and import
-            funcroot = which('AirwayCNN');
-            [modulepath,~,~] = fileparts(funcroot);
+            GANCNNpath = fullfile(modulepath,'GANCNN');
             pyrun(['import sys; sys.path.append("', modulepath,'");']);
+            pyrun(['import sys; sys.path.append("', GANCNNpath,'");']);
             mod = py.importlib.import_module('AQ_CNR');
             py.importlib.reload(mod);
 
             pycode = ...
                 ['import sys; sys.path.append("', modulepath,'");', ...
-                ' from AQ_CNN import runCNNmodel;',...
+                ' from AQ_CNR import ',subtype, ';',...
                 ' modelpath = "', model_path, '";', ...
                 ' data_path = "', temp_dir, '";', ...
                 ' batch_size = ', num2str(256), ';', ...
                 ' mean = ', num2str(meanval), ';', ...
                 ' var = ', num2str(varval), ';', ...
-                ' out = runCNNmodel(modelpath, data_path, batch_size, mean, var)'];
-
+                ' out = ',subtype,'(modelpath, data_path, batch_size, mean, var)'];
+            
             pymeasures = pyrun(pycode, 'out');
-            measures = double(pymeasures{1});
+            measures = double(pymeasures);
 
             % set up memory
             allslices = 1:length(obj.tube.source);
@@ -55,7 +81,7 @@ classdef AirwayCNN < SuperMeasure
             total_slices_sz = length(allslices);
             ellipse_inner = cell(total_slices_sz, 1);
             ellipse_outer = cell(total_slices_sz, 1);
-
+            
             initslice = chosenslices(1);
 
             for k = 1:size(measures,1)
@@ -84,9 +110,9 @@ classdef AirwayCNN < SuperMeasure
             obj.measures = cell(2, length(ellipse_inner));
             obj.measures(1,:) = ellipse_inner;
             obj.measures(2,:) = ellipse_outer;
-
+            
         end
 
-
+        
     end
 end
